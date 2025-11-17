fc18c7c2f7d1c949bfef7f14b152bd83c87b66dd
Jiyo P V <jiyopv21@gmail.com>
2025-11-17 21:02:58 +0530

    modified:   cla2.class 	modified:   cla2.java

--- cla2.java
+++ cla2.java
@@ -1,49 +1,59 @@
-import java.io.*;
-class cla2{
-	public static void main(String args[])
-	{
-		int res=0;
-		if(args.length!=3)
-		{
-				System.out.println("error: need 3 argouments \ncla2 a b <opcode>");
-				System.out.println("op codes:0-add 1-diff 2-multipy 3-divide");
-				
-				return;
-			
-		}	
-		int a=Integer.parseInt(args[0]);
-		int b=Integer.parseInt(args[1]);
-		switch(args[2])
-		{
-			case "0":
-				System.out.println("addition");
-				res=a+b;	
-				break;
-			
-			case "1":
+// Program to demonstrate arithmetic operations using command-line arguments in Java
+// @JIYO P V    Roll no:33
+// 17 November 2025
 
-				System.out.println("Subtraction");
-				res=a-b;	
-				break;
+class cla2 {
+    public static void main(String args[]) {
 
-			case "2":
+        int res = 0; // variable to store result
 
-				System.out.println("product");
-				res=a*b;	
-				break;
+        // Check if exactly 3 arguments are passed
+        if (args.length != 3) {
+            System.out.println("error: need 3 arguments \ncla2 a b <opcode>");
+            System.out.println("op codes:0-add 1-diff 2-multiply 3-divide");
+            return;
+        }
 
-			case "3":
+        // Convert first two arguments to integers
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
 
-				System.out.println("division");
-				if(b==0)
-				{
-					System.out.println("division by zero not possible");
-					return;	
-				}	
-				res=a/b;
-				break;
-		}
-		
-		System.out.println(res);
-	}
+        // Switch based on opcode (third argument)
+        switch (args[2]) {
+
+            case "0":
+                System.out.println("addition");
+                res = a + b; // perform addition
+                break;
+
+            case "1":
+                System.out.println("Subtraction");
+                res = a - b; // perform subtraction
+                break;
+
+            case "2":
+                System.out.println("product");
+                res = a * b; // perform multiplication
+                break;
+
+            case "3":
+                System.out.println("division");
+
+                // Check division by zero
+                if (b == 0) {
+                    System.out.println("division by zero not possible");
+                    return;
+                }
+                res = a / b; // perform division
+                break;
+
+            default:
+                // Invalid opcode
+                System.out.println("error: opcodes are 0 1 2 3");
+                return;
+        }
+
+        // Print final result
+        System.out.println(res);
+    }
 }
