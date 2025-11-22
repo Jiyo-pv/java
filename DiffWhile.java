7e9f71b62e804a6e65e7147fb9335197c7faf0f2
Jiyo P V <jiyopv21@gmail.com>
2025-11-22 18:33:50 +0530

    added

---
+++ DiffWhile.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;           //diff of 2 nos using while loop
+class DiffWhile
+{
+
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b,c,max=0,i;
+        System.out.println("enter 2 values\n");
+        a=sc.nextInt();
+        b=sc.nextInt();
+        i=a;
+        max=b;
+
+        
+
+    }
+}
