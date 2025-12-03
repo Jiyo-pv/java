2a8fdb56de3906636d6168a9a036ea8b2acb0bbd
Jiyo P V <jiyopv21@gmail.com>
2025-12-03 21:37:25 +0530

    added

--- NRecursive.java
+++ NRecursive.java
@@ -1,7 +1,8 @@
 //Display from n - 1 and 1-n using recursive functions.
+import java.util.*;
 class NRecursive
 {
-    void nto1(n)
+    void nto1(int n)
     {
         if(n==1)
         {
@@ -12,12 +13,26 @@ class NRecursive
             nto1(n-1);
         }
     }
-   
+   void ton(int n)
+    {
+        if(n==0)
+        {
+            return;
+        }    
+       ton(n-1);
+       System.out.println(n);
+       
+    }
     public static  void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
         System.out.print("enter n:");
         int n=sc.nextInt();
-
+        NRecursive x=new NRecursive();
+         System.out.println("n to 1");
+        x.nto1(n);
+        
+        System.out.println("1 to n");
+        x.ton(n);
     }
 }
