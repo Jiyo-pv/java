0a04df010a6fe98c391517afc8df0d6631ad4652
Jiyo P V <jiyopv21@gmail.com>
2025-12-04 22:03:33 +0530

    added at 04-12-2025 22:03:32.99

---
+++ Student.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class StudentInfo
+{
+    int sno;
+    String sname,sprogram;
+    public void read()
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter  student no:");
+        sno=sc.nextInt();
+        System.out.print("enter  student name:");
+        sname=sc.next();
+        System.out.print("enter  program:");
+        sprogram=sc.next();
+    }
+    public void disp()
+    {
+        System.out.println("\nsno:"+sno);
+        System.out.println("name:"+sname);
+        System.out.println("program:"+sprogram);
+    }
+}
+class Student
+{
+    public static  void main(String args[])
+    {
+        StudentInfo s1,s2;
+        s1=new StudentInfo();
+        s2=new StudentInfo();
+        s1.read();
+        s2.read();
+        s1.disp();
+        s2.disp();
+
+    }
+}
