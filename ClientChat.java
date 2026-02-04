94bad7b285c6688a24823c181cc52e01d7bd22d4
Jiyo P V <jiyopv21@gmail.com>
2026-02-04 21:56:03 +0530

    added at 04-02-2026 21:56:03.41

---
+++ ClientChat.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.net.*;
+class ClienChat
+{
+    public static void main(String args[])
+    {
+        try{
+            Socket s=new Socket("localhost",1234);
+            DataInputStream sin=new DataInputStream(s.getInputStream());
+            DataOutputStream sout=new DataOutputStream(s.getOutputStream());
+            DataInputStream stdin=new DataInputStream(System.in);
+            String str="";
+            while(true)
+            {
+                str=sin.readUTF();
+                System.out.println("Server says:"+str);
+                if(str.equals("quit"))
+                    break;
+                System.out.print("Enter message:");
+                str=stdin.readLine();
+                sout.writeUTF(str);
+                if(str.equals("quit"))
+                    break;
+
+                    
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+}
