cd5f5ad8b3e38bb1348f5f063e48fa08d411b92f
Jiyo P V <jiyopv21@gmail.com>
2026-02-05 22:13:21 +0530

    added at 05-02-2026 22:13:20.94

--- ServerChat.java
+++ ServerChat.java
@@ -9,8 +9,24 @@ class ServerChat
             ServerSocket ss=new ServerSocket(1234);
             System.out.println("Server is ready to accept connection");
             Socket s=ss.accept();
-            System.out.println("Client connected");
-            DataInputStream sin=new DataInputStream(s.getInputStream());
+            System.out.println("Client connected"+s);
+            new ClientHandler(s).start();
+        }catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+}
+class ClientHandler extends Thread
+{
+    Socket s;
+    ClientHandler(Socket s)
+    {
+        this.s=s;
+    }
+    public void run()
+    {
+        DataInputStream sin=new DataInputStream(s.getInputStream());
             DataOutputStream sout=new DataOutputStream(s.getOutputStream());
             DataInputStream stdin=new DataInputStream(System.in);
             String str="welcome to server";
@@ -28,9 +44,5 @@ class ServerChat
 
 
             }
-        }catch(Exception e)
-        {
-            System.out.println(e);
-        }
     }
 }
